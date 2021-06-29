78f1c7e0f4a069a8a8141c0e1208969d882d7d85
John Orfanakos <yianni312@gmail.com>
2021-06-29 11:19:57 -0400

    list component

--- src/Card/Create/index.js
+++ src/Card/Create/index.js
@@ -3,50 +3,49 @@ import { Link, useHistory, useParams } from "react-router-dom";
 import { createCard, readDeck } from "../../utils/api";
 import CardForm from "../Form";
 
+function CardCreate() {
+  const history = useHistory();
+  const { deckId } = useParams(); //destructuing deckId from useParams
+  const [deck, setDeck] = useState({ cards: [] });
 
-function CardCreate(){
-    const history = useHistory();
-    const { deckId } = useParams();//destructuing deckId from useParams
-    const [deck, setDeck] = useState({cards: []});
+  useEffect(() => {
+    readDeck(deckId).then(setDeck);
+  }, [deckId]);
 
-    useEffect(()=>{
-        readDeck(deckId).then(setDeck);
-    },[deckId]);
+  function submitHandler(card) {
+    //function created to handle submits, run createCard with deckId as a param
+    createCard(deckId, card);
+  }
 
-    function submitHandler(card){
-        createCard(deckId, card);
-    }
+  function doneHandler() {
+    history.push(`/decks/${deckId}`);
+  }
 
-    function doneHandler(){
-        history.push(`/decks/${deckId}`);
-    }
-    
-    return(
-        <div>
-        <nav aria-label="breadcrumb">
-          <ol className="breadcrumb">
-            <li className="breadcrumb-item">
-              <Link to="/">
-                <span className="oi oi-home" /> Home
-              </Link>
-            </li>
-            <li className="breadcrumb-item">
-              <Link to={`/decks/${deckId}`}>{deck.name}</Link>
-            </li>
-            <li className="breadcrumb-item active" aria-current="page">
-              Add Card
-            </li>
-          </ol>
-        </nav>
-        <CardForm
-          deckName={deck.name}
-          initialState={deck}
-          onSubmit={submitHandler}
-          onDone={doneHandler}
-        />
-      </div>
-    )
-
+  return (
+    <div>
+      <nav aria-label="breadcrumb">
+        <ol className="breadcrumb">
+          <li className="breadcrumb-item">
+            <Link to="/">
+              <span className="oi oi-home" /> Home
+            </Link>
+          </li>
+          <li className="breadcrumb-item">
+            <Link to={`/decks/${deckId}`}>{deck.name}</Link>
+          </li>
+          <li className="breadcrumb-item active" aria-current="page">
+            Add Card
+          </li>
+        </ol>
+      </nav>
+      <CardForm
+        deckName={deck.name}
+        initialState={deck}
+        onSubmit={submitHandler}
+        onDone={doneHandler}
+      />
+    </div>
+  );
 }
 
 export default CardCreate;
